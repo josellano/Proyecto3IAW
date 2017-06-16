5d5d9829c43b92988e29d081aef2477b174825fa
LLANITO\jose <jose_llano_95@hotmail.com>
2017-06-16 18:42:56 -0300

    cargado de borrador del proyecto.

---
+++ loginapp/app.js
@@ -0,0 +1,108 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+
+var morgan= require('morgan');
+var methodOverride  = require('method-override');
+//var modernizr = require("modernizr");
+
+mongoose.connect('mongodb://localhost/loginapp');
+var db = mongoose.connection;
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+// Init App
+var app = express();
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout:'layout'}));
+app.set('view engine', 'handlebars');
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Set Static Folder
+//app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
+
+
+
+app.use('/', routes);
+app.use('/users', users);
+// Routes del mapa
+// ------------------------------------------------------
+require('./app/routes.js')(app);
+
+// Set Port
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function(){
+	console.log('Server started on port '+app.get('port'));
+});
+
+
+// Logging and Parsing
+app.use(express.static(__dirname + '/public'));                 // sets the static files location to public
+app.use('/public/css',express.static(path.join(__dirname, '/public/css')));
+app.use('/public/source',express.static(path.join(__dirname, '/public/source')));
+app.use('/public/fonts',express.static(path.join(__dirname, '/public/fonts')));
+app.use('/bower_components',  express.static(__dirname + '/bower_components')); // Use BowerComponents
+app.use(morgan('dev'));                                         // log with Morgan
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.urlencoded({extended: true}));               // parse application/x-www-form-urlencoded
+app.use(bodyParser.text());                                     // allows bodyParser to look at raw text
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));  // parse application/vnd.api+json as json
+app.use(methodOverride());
